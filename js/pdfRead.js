508c11ff329bd252fc9f4c04e46dc093a620c305
Plut0 <slimdoks@gmail.com>
2020-02-03 23:02:59 +0900

    [feat] pdfRead

--- js/pdfRead.js
+++ js/pdfRead.js
@@ -9,11 +9,18 @@ String.prototype.replaceAll = function(org,dest){
 var inputElement = document.getElementById("pdfFile");
 inputElement.addEventListener("change", handleFiles, false);
 
+let part_weight = []
+let part_name = []
+let file_name = ""
+exports.part_weight = part_weight;
+exports.part_name = part_name;
+
 async function handleFiles(file) {
+	// console.log("handle File")
     let remove_table = document.querySelectorAll('.part_content tr');
     for(var i=1;i<remove_table.length;i++){
-      console.log(remove_table[i]);
-      remove_table[i].remove();
+		// console.log(remove_table[i]);
+		remove_table[i].remove();
     }
 
     const pdfFile = this.files[0];
@@ -22,20 +29,44 @@ async function handleFiles(file) {
     let dataBuffer = pdfFile.path;
     dataBuffer = dataBuffer.replaceAll('\\','/');
 
-    var data = await main.pdfParsing(dataBuffer);
+	var data = await main.pdfParsing(dataBuffer);
+	file_name = data[0]
+	exports.file_name = file_name;
+// console.log(file_name)
     console.log(pdfFile.name);
-    for(var i=0;i<data.length;i+=2){
-      let addPart = document.createElement("tr");
-      let partname = document.createElement("td");
-      let count = document.createElement("td");
-      let input = document.createElement("input");
+    for(var i=1;i<data.length;i+=2){
+		let addPart = document.createElement("tr");
+		let partname = document.createElement("td");
+		let count = document.createElement("td");
+		let input = document.createElement("input");
 
-      input.id = `part_${i}`
-      partname.appendChild(document.createTextNode(data[i]));
-      count.appendChild(input);
-      addPart.appendChild(partname);
-      addPart.appendChild(count);
+		input.id = `part_${parseInt(i/2)}`
+		part_weight.push(data[i+1])
+		part_name.push(data[i])
+		partname.appendChild(document.createTextNode(data[i]));
+		count.appendChild(input);
+		addPart.appendChild(partname);
+		addPart.appendChild(count);
 
-      document.getElementById("part_content").appendChild(addPart);
+		document.getElementById("part_content").appendChild(addPart);
     }
+	part_weight = await weight_modify(part_weight)
+    console.log(part_weight)
+}
+
+function weight_modify(part_weight){
+
+    for(var i=0;i<part_weight.length;i++){
+		part_weight[i] = part_weight[i].split(" ")[0]*1
+		// console.log(part_weight[i])
+		if(part_weight[i]%5000 != 0){
+			part_weight[i] -= part_weight[i]%5000
+			part_weight[i] += 5000
+			part_weight[i] = parseInt(part_weight[i]/1000)
+		}
+		else{
+			part_weight[i] = parseInt(part_weight[i]/1000)
+		}
+	}
+	return part_weight
 }
